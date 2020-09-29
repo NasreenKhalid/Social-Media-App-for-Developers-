0be6b661a8bd15688fe02ef9c8ac9cfcab45d9a9
Nasreen Khalid <nasreen.1@live.com>
2020-09-29 23:09:58 +0300

    User register and login routes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,81 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator");
+const User = require("../../models/User");
 
 // Test route api/auth -- Public route
-router.get("/", (req, res) => res.send("Auth route"));
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+// Authenticate user & get token to login
+router.post(
+  "/",
+  [
+    check("email", "Please include a valid email address").isEmail(),
+    check("password", "Password is required").exists(),
+  ],
+  async (req, res) => {
+    // console.log(req.body);
+    const errors = validationResult(req);
+    //  if there r errors we do the following:
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      // See if user exists so we'll send error while registering
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials" }] });
+      }
+
+      //   We need to make sure that the pwd matches to email
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid credentials" }] });
+      }
+
+      // Return jsonwebtoken so that when user registers successfully they get logged in right away
+      const payload = {
+        user: {
+          id: user.id, // id = _id from mongodb
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          console.log(token);
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 module.exports = router;
