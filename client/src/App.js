14c928243307bf55733a0df471314df27fede500
Nasreen Khalid <nasreen.1@live.com>
2020-10-07 17:18:36 +0300

    Frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,47 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 import Landing from "./components/layout/Landing";
+import Alert from "./components/layout/Alert";
+// Redux
+import { Provider } from "react-redux";
+import store from "./store.js";
+import { loadUser } from "./actions/auth";
 
 import "./App.css";
+import setAuthToken from "./utils/setAuthToken";
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+// The useEffect hook keeps running always when teh state updates
+// unless we add an empty bracket as 2nd parameter which will
+// make it run only once
 
 const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
-    <Fragment>
-      <Navbar />
-      <Landing />
-    </Fragment>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <section className="container">
+            <Alert />
+            <Switch>
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 };
 
---
+++ client/src/actions/types.js
@@ -0,0 +1,10 @@
+// we are defining the action types here (ina a separate file) to be used by the reducers
+export const SET_ALERT = "SET_ALERT";
+export const REMOVE_ALERT = "REMOVE_ALERT";
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAIL = "REGISTER-FAIL";
+export const USER_LOADED = "USER_LOADED";
+export const AUTH_ERROR = "AUTH_ERROR";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAIL = "LOGIN_FAIL";
+export const LOGOUT = "LOGOUT";
