3b9371519baccaf95e008b3d807ccce1b788f181
Nasreen Khalid <nasreen.1@live.com>
2020-10-19 16:50:35 +0300

    Comments Functionality

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -5,7 +5,10 @@ import {
     POST_ERROR,
     UPDATE_LIKES,
     DELETE_POST,
-    ADD_POST
+    ADD_POST,
+    GET_POST,
+    ADD_COMMENT,
+    REMOVE_COMMENT
 } from './types'
 
 // Get posts
@@ -89,7 +92,7 @@ export const addPost =formData=> async dispatch => {
   }
   
     try {
-     const res=  await axios.post('/api/posts', formData)
+     const res=  await axios.post('/api/posts', formData, config)
 
         dispatch({
             type:ADD_POST,
@@ -97,6 +100,68 @@ export const addPost =formData=> async dispatch => {
         });
 
         dispatch(setAlert('Post Created' , 'success'))
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: {msg: err.response.statusText, status: err.response.status}
+        })
+    }
+}
+
+// Add Comment
+export const addComment =(postId, formData)=> async dispatch => {
+  const config = {
+      headers :{
+          'Content-Type': 'application/json'
+      }
+  }
+  
+    try {
+     const res=  await axios.post(`/api/posts/comment/${postId}`, formData,config)
+
+        dispatch({
+            type:ADD_COMMENT,
+            payload:res.data
+        });
+
+        dispatch(setAlert('Comment Added' , 'success'))
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: {msg: err.response.statusText, status: err.response.status}
+        })
+    }
+}
+
+// Delete Comment
+export const deleteComment =(postId, commentId)=> async dispatch => {
+       
+      try {
+       const res=  await axios.delete(`/api/posts/comment/${postId}/${commentId}`)
+  
+          dispatch({
+              type:REMOVE_COMMENT,
+              payload:commentId
+          });
+  
+          dispatch(setAlert('Comment Deleted' , 'success'))
+      } catch (err) {
+          dispatch({
+              type: POST_ERROR,
+              payload: {msg: err.response.statusText, status: err.response.status}
+          })
+      }
+  }
+
+// Get post -- To view a single post and any comments on that post
+export const getPost = id => async dispatch => {
+    try {
+        const res = await axios.get(`/api/posts/${id}`)
+
+        dispatch({
+            type:GET_POST,
+            payload:res.data
+        })
     } catch (err) {
         dispatch({
             type: POST_ERROR,
--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -5,7 +5,7 @@ import Moment from 'react-moment'
 import {connect} from 'react-redux'
 import {addLike, removeLike, deletePost} from '../../actions/post'
 
-const PostItem =({addLike, removeLike,deletePost, auth,post: {_id, text, name, avatar,user, likes,comments, date}}) => {
+const PostItem =({addLike, removeLike,deletePost, auth,post: {_id, text, name, avatar,user, likes,comments, date}, showActions}) => {
     return (
         <div className="post bg-white p-1 my-1">
         <div>
@@ -24,7 +24,8 @@ const PostItem =({addLike, removeLike,deletePost, auth,post: {_id, text, name, a
            <p className="post-date">
               Posted on: <Moment format="YYYY/MM/DD">{date}</Moment>
           </p>
-          <button onClick={e => addLike(_id)} type="button" className="btn btn-light">
+          {showActions && <Fragment>
+            <button onClick={e => addLike(_id)} type="button" className="btn btn-light">
             <i className="fas fa-thumbs-up"></i>{ ' '}
             <span>{likes.length > 0 && (
                 <span className='comment-count'>{likes.length}</span>
@@ -33,7 +34,7 @@ const PostItem =({addLike, removeLike,deletePost, auth,post: {_id, text, name, a
           <button onClick={e => removeLike(_id)} type="button" className="btn btn-light">
             <i className="fas fa-thumbs-down"></i>
           </button>
-          <Link to={`/post/${_id}`} className="btn btn-primary">
+          <Link to={`/posts/${_id}`} className="btn btn-primary">
             Discussion {comments.length > 0 && (
                 <span className='comment-count'>{comments.length}</span>
             )}
@@ -48,10 +49,18 @@ const PostItem =({addLike, removeLike,deletePost, auth,post: {_id, text, name, a
           <i className="fas fa-times"></i>
         </button>
           )}
-          
+
+          </Fragment>}
+                   
         </div>
         </div>
-    )
+    );
+
+   
+}
+
+PostItem.defaultProps = {
+  showActions: true
 }
 
 PostItem.propTypes = {
